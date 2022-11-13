8203adf2f9248b856612ab69842807dcee744326
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-11-14 00:24:11 +0600

    login and sign up with firebase

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,69 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+
+  const { login } = useContext(AuthContext);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    login(data.email, data.password)
+      .then((res) => console.log(res.user))
+      .catch((err) => console.log(err));
+  };
   return (
-    <div>
-      <h1>Login</h1>
+    <div className="flex justify-center items-center w-full  h-[800px]">
+      <div className="shadow-md p-7 rounded-2xl  w-96">
+        <h2 className="text-xl text-center">Login</h2>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Your email"
+              className="input input-bordered w-full"
+              {...register("email")}
+            />
+          </div>
+          <div className="form-control w-full">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Your password"
+              className="input input-bordered w-full"
+              {...register("password")}
+            />
+            <label className="label">
+              <Link className="label-text-alt">Forgot Password?</Link>
+            </label>
+          </div>
+
+          <input
+            type="submit"
+            value={"Login"}
+            className="input input-bordered w-full bg-accent text-white"
+          />
+          <label className="label justify-center">
+            <span className="label-text-alt ">
+              New to Doctors Portal?{" "}
+              <Link className="text-secondary" to={"/register"}>
+                Create New Account
+              </Link>
+            </span>
+          </label>
+        </form>
+
+        <div className="divider">OR</div>
+        <button className="uppercase btn btn-outline w-full">
+          Continue with google
+        </button>
+      </div>
     </div>
   );
 };
---
+++ src/pages/SignUp/SignUp.jsx
@@ -0,0 +1,83 @@
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
+const SignUp = () => {
+  const { register, handleSubmit } = useForm();
+  const { createUser } = useContext(AuthContext);
+
+  const onSubmit = (data) => {
+    console.log(data);
+    createUser(data.email, data.password)
+      .then((res) => console.log(res.user))
+      .catch((err) => console.log(err));
+  };
+  return (
+    <div>
+      <div className="flex justify-center items-center w-full  h-[800px]">
+        <div className="shadow-md p-7 rounded-2xl  w-96">
+          <h2 className="text-xl text-center">Sign Up</h2>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="Your name"
+                className="input input-bordered w-full"
+                {...register("name")}
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="Your email"
+                className="input input-bordered w-full"
+                {...register("email")}
+              />
+            </div>
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="Your password"
+                className="input input-bordered w-full"
+                {...register("password")}
+              />
+              <label className="label">
+                <Link className="label-text-alt">Forgot Password?</Link>
+              </label>
+            </div>
+
+            <input
+              type="submit"
+              value={"Login"}
+              className="input input-bordered w-full bg-accent text-white"
+            />
+            <label className="label justify-center">
+              <span className="label-text-alt ">
+                Already have an account?{" "}
+                <Link className="text-secondary" to={"/login"}>
+                  Login
+                </Link>
+              </span>
+            </label>
+          </form>
+
+          <div className="divider">OR</div>
+          <button className="uppercase btn btn-outline w-full">
+            Continue with google
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
